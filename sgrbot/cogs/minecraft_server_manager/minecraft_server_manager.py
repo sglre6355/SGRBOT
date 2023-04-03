f5223dbc5580f7f448570d866d9b2be9a0b6293c
sglre6355 <sglre6355@gmail.com>
2023-04-03 23:17:45 +0100

    fix: Fix an error handling bug in minecraft commands
    Setting `is_processing` to `False` in `minecraft_error` should be done
    after checking whether the error is raised by
    `abort_if_processing_previous_command` or not.

--- sgrbot/cogs/minecraft_server_manager/minecraft_server_manager.py
+++ sgrbot/cogs/minecraft_server_manager/minecraft_server_manager.py
@@ -154,15 +154,6 @@ class MinecraftServerManager(commands.Cog):
     @stop.error
     @restart.error
     async def minecraft_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError) -> None:
-        # Set is_processing to False to allow other commands to be run after an error
-        if hasattr(error, "server"):
-            error.server.is_processing = False
-
-        if isinstance(error, ServerStartFailed):
-            start_failed_embed = MinecraftServerEmbed(MinecraftServerEmbedFormats.start_failed, server=error.server)
-            await interaction.edit_original_response(embed=start_failed_embed)
-            return
-
         if isinstance(error, InvalidServerName):
             invalid_server_name_embed = MinecraftServerEmbed(MinecraftServerEmbedFormats.invalid_server_name, server_name=error.server_name)
             await interaction.response.send_message(embed=invalid_server_name_embed)
@@ -173,6 +164,10 @@ class MinecraftServerManager(commands.Cog):
             await interaction.response.send_message(embed=server_processing_embed)
             return
 
+        # Set is_processing to False to allow other commands to be run after an error
+        if hasattr(error, "server"):
+            error.server.is_processing = False
+
         if isinstance(error, ServerAlreadyRunning):
             already_running_embed = MinecraftServerEmbed(MinecraftServerEmbedFormats.already_running, server=error.server)
             await interaction.response.send_message(embed=already_running_embed)
@@ -181,6 +176,11 @@ class MinecraftServerManager(commands.Cog):
         if isinstance(error, ServerNotRunning):
             not_running_embed = MinecraftServerEmbed(MinecraftServerEmbedFormats.not_running, server=error.server)
             await interaction.response.send_message(embed=not_running_embed)
+            return
+
+        if isinstance(error, ServerStartFailed):
+            start_failed_embed = MinecraftServerEmbed(MinecraftServerEmbedFormats.start_failed, server=error.server)
+            await interaction.edit_original_response(embed=start_failed_embed)
             return
 
         raise error
